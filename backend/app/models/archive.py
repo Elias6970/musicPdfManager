bd8e4cbd047ead7dc3afa9b8ff5fac5b4a1faaf1
elias6970 <eliasiborra12@gmail.com>
2026-03-11 14:17:05 +0100

    Created model for archive

---
+++ backend/app/models/archive.py
@@ -0,0 +1,23 @@
+from typing import Optional, List, TYPE_CHECKING
+from sqlmodel import SQLModel, Field, Relationship
+
+if TYPE_CHECKING:
+    from backend.app.models.piece import Piece
+    from backend.app.models.user_archive_link import UserArchiveLink
+
+
+class ArchiveBase(SQLModel):
+    name: str
+
+class ArchiveCreate(ArchiveBase):
+    pass
+
+class ArchivePublic(ArchiveBase):
+    id: int
+
+class Archive(ArchiveBase, table=True):
+    __tablename__ = "archives"
+    id: Optional[int] = Field(default=None, primary_key=True)
+    
+    pieces: List["Piece"] = Relationship(back_populates="archive")
+    user_links: List["UserArchiveLink"] = Relationship(back_populates="archive")
--- backend/app/models/piece.py
+++ backend/app/models/piece.py
@@ -4,6 +4,7 @@ from sqlmodel import SQLModel, Field, Relationship
 if TYPE_CHECKING:
     from backend.app.models.author import Author
     from backend.app.models.type import Type
+    from backend.app.models.archive import Archive
 
 class PieceBase(SQLModel):
     cod: int
@@ -15,6 +16,7 @@ class PieceBase(SQLModel):
 class PieceCreate(PieceBase):
     author_id: int
     type_id: int
+    archive_id: int
 
 class PiecePublic(PieceBase):
     id: int
@@ -23,12 +25,14 @@ class Piece(PieceBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     author_id: Optional[int] = Field(default=None, foreign_key="author.id")
     type_id: Optional[int] = Field(default=None, foreign_key="type.id")
+    archive_id: Optional[int] = Field(default=None, foreign_key="archive.id")
     
     author: Optional["Author"] = Relationship(back_populates="pieces")
     type: Optional["Type"] = Relationship(back_populates="pieces")
+    archive: Optional["Archive"] = Relationship(back_populates="pieces")
     
     @property
-    def cod_name(self) -> str:
+    def std_name(self) -> str:
         """Dynamically concatenated cod and name."""
         return f"{self.cod}-{self.name}"
 
