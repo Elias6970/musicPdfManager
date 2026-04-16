084ad91b35f85eaee58c268dcb6c4680ba8bb2fe
elias6970 <eliasiborra12@gmail.com>
2026-04-16 12:53:48 +0200

    Individual selector with initial data

--- frontend/pyqt/app/selectors/individual_selection_controller.py
+++ frontend/pyqt/app/selectors/individual_selection_controller.py
@@ -62,6 +62,8 @@ class IndividualSelectionController(QObject):
         self.preview_controller.enable_previous.connect(self.view.btn_mv_back_preview.setEnabled)
         self.preview_controller.enable_next.connect(self.view.btn_mv_forward_preview.setEnabled)
 
+        self.refresh()
+        
     def generate_pdf(self):
         """
         Generate the PDF with the added scores.
